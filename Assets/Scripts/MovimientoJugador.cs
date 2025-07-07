19fd15f43405e74c75cc0b4da73911be8506e35e
JuegosTerva <juegosterva@gmail.com>
2025-07-07 12:32:26 -0600

    v25.07.07

--- Assets/Scripts/MovimientoJugador.cs
+++ Assets/Scripts/MovimientoJugador.cs
@@ -3,22 +3,63 @@ using UnityEngine.InputSystem;
 
 public class MovimientoJugador : MonoBehaviour
 {
+    public float velocidad = 5f;
+    Rigidbody2D rb;
+    public Vector2 entrada;
+    public Animator animator;
+    public GameObject preFabTrigo;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.linearVelocity = entrada * velocidad;
     }
 
-    public void Moverse(InputAction.CallbackContext entradaTeclado){
-        if(entradaTeclado.performed){
-            Debug.Log("Hola Mundo");
-        }
+    public void Moverse(InputAction.CallbackContext contexto){
         
+        Vector2 valorEntrada = contexto.ReadValue<Vector2>();
+
+        // Determinar el eje dominante
+        if (Mathf.Abs(valorEntrada.x) > Mathf.Abs(valorEntrada.y))
+        {
+            // Movimiento horizontal
+            entrada = new Vector2(Mathf.Sign(valorEntrada.x), 0);
+        }
+        else if (Mathf.Abs(valorEntrada.y) > 0)
+        {
+            // Movimiento vertical
+            entrada = new Vector2(0, Mathf.Sign(valorEntrada.y));
+        }
+        else
+        {
+            entrada = Vector2.zero;
+        }
+
+        // Actualiza los parámetros del blend tree
+        animator.SetFloat("entradaX", entrada.x);
+        animator.SetFloat("entradaY", entrada.y);
+
+        if (contexto.performed)
+        {
+            animator.SetBool("caminando", true);
+        }
+        else if (contexto.canceled)
+        {
+            animator.SetBool("caminando", false);
+        }
+    }
+
+    public void SembrarTrigo(InputAction.CallbackContext contexto){
+     if(contexto.started) {
+        Instantiate(preFabTrigo ,transform.position, Quaternion.identity);
+     }  
     }
 }
